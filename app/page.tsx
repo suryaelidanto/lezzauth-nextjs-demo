cebe142550ce2e928d632deccf44b1e9ad545467
suryaelidanto <suryaelidanto@gmail.com>
2023-12-08 14:16:20 +0700

    feat: adding userbutton implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { UserButton } from "@/lezzauth/_generated/components/user-button/UserButton"
 import { useUser } from "lezzauth/nextjs"
 
 export default function Page() {
     const { user } = useUser()
 
     return (
-        <h1>Hello {user.firstName} {":)"}</h1>
+        <div className="flex justify-between p-3">
+            <h1>Hello {user.firstName} {":)"}</h1>
+            <UserButton />
+        </div>
     )
 }
